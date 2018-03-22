b62e1a1679184b5f642200345839802c34a35c03
Israel Medina <israel.medina@salesforce.com>
2018-03-22 18:12:26 -0300

    Added reconnection loop

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 // app.js
-var express = require('express');  
-var app = express();  
-var server = require('http').createServer(app);  
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var telnet = require('telnet-client');
 var tconnect = new telnet();
 
-app.use(express.static(__dirname + '/node_modules'));  
+app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname + '/assets'));
 
-app.get('/', function(req, res,next) {  
+app.get('/', function(req, res,next) {
     res.sendFile(__dirname + '/index.html');
 });
 
 var tparams = {
-  host: '127.0.0.1',
+  host: '192.168.1.10',
   port: 9800,
   shellPrompt: '/ # ',
   timeout: 1500,
-  // removeEcho: 4 
+  // removeEcho: 4
 };
 
 
@@ -26,7 +26,7 @@ function changeinst(inst) {
     var flcmd = 'prog 0 ' + inst;
     console.log("fluidsynth: ", flcmd);
     tconnect.send(flcmd, function(err, response) {
-       // console.log(response);  
+       // console.log(response);
     });
 }
 
@@ -37,29 +37,39 @@ function currentinstruments() {
       });
       console.log(current);
       io.emit('message', { cord: current })
-} 
+}
 
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
-
+function telnetConnect(){
+  console.log("Fluidsynth Telnet: connecting...");
+  tconnect.connect(tparams).then(function () {
+    console.log("Fluidsynth Telnet: connection success");
+  });
+}
 io.on('connection', function(client) {
     console.log('server connected');
-    tconnect.connect(tparams);
-    tconnect.send('inst 1', function(err, ins) {
-            client.emit('instrumentdump', { package: ins });
-        });
+    telnetConnect();
+
+    tconnect.on('error', function (err) {
+      console.error("Fluidsynth Telnet: connection failed, retrying in 5 seconds...");
+      setTimeout(telnetConnect, 5000);
+    });
+
     client.on('message', function(data) {
         if (isNumeric(data)) {
-		      changeinst(data);  
+		      changeinst(data);
         } else if (data == "list") {
           tconnect.send('channels', function(err, ins) {
             io.emit('current', { package: ins });
-          });  
+          });
         } else {
-		      console.log(data);
+		      //console.log(data);
         }
     });
 });
-
+io.on('error', function(error) {
+  console.log(error);
+});
 server.listen(7000);
