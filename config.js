0a84e07d7c5fd3ca753e70c39357b3fe764315cf
Israel Medina <isra2dave@gmail.com>
2018-03-22 19:00:31 -0300

    Fix for bad object formation

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
-  FLUIDSYNTH_HOST: process.env.FLUIDSYNTH_HOST || '127.0.0.1';
-  FLUIDSYNTH_PORT: process.env.FLUIDSYNTH_PORT || 9800;
-  FLUIDSYNTH_TIMEOUT: process.env.FLUIDSYNTH_TIMEOUT || 1500;
+  FLUIDSYNTH_HOST: process.env.FLUIDSYNTH_HOST || '127.0.0.1',
+  FLUIDSYNTH_PORT: process.env.FLUIDSYNTH_PORT || 9800,
+  FLUIDSYNTH_TIMEOUT: process.env.FLUIDSYNTH_TIMEOUT || 1500
 }
 
 module.exports = config;
